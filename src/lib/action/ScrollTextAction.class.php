dff70792eea92866e1a076517a1a643503a6a9f0
Jim Martens <jim1@live.de>
2013-08-01 16:28:28 +0200

    Adjusted validation pattern. Added AJAX request to add new items.

---
+++ src/lib/action/ScrollTextAction.class.php
@@ -0,0 +1,109 @@
+<?php
+// wcf imports
+require_once(WCF_DIR.'lib/action/AbstractSecureAction.class.php');
+
+/**
+ * Inserts new items into the database.
+ * 
+ * @author		Jim Martens
+ * @copyright	2013 Jim Martens
+ * @license		http://www.gnu.org/licenses/lgpl-3.0 GNU Lesser General Public License, version 3
+ * @package		de.plugins-zum-selberbauen.scrollingText
+ * @subpackage	action
+ * @category	Community Framework
+ */
+class ScrollTextAction extends AbstractSecureAction {
+	/**
+	 * the response
+	 * @var string
+	 */
+	protected $response = '';
+	
+	/**
+	 * the value of the new item
+	 * @var string
+	 */
+	protected $newItem = '';
+	
+	/**
+	 * @see Action::execute()
+	 */
+	public function execute() {
+		parent::execute();
+		$this->saveToDatabase();
+		$this->resetCache();
+		$this->generateResponse();				
+		$this->sendResponse();
+	}
+	
+	/**
+	 * @see Action::readParameters()
+	 */
+	public function readParameters() {
+		parent::readParameters();
+		if (isset($_POST['value'])) $this->newItem = StringUtil::trim($_POST['value']);
+	}
+	
+	/**
+	 * Saves the new item into database.
+	 */
+	protected function saveToDatabase() {
+		if (empty($this->newItem)) {
+			$this->throwException();
+		}
+		
+		$sql = 'INSERT INTO wcf'.WCF_N.'_texts
+		        (text, created)
+		        VALUES
+		        (:text, :time)';
+		$sql = str_replace(':text', "'".escapeString($this->newItem)."'", $sql);
+		$sql = str_replace(':time', TIME_NOW);
+		WCF::getDB()->sendQuery($sql);
+	}
+	
+	/**
+	 * Resets the cache.
+	 */
+	protected function resetCache() {
+		$cache = 'scrollText';
+		$cacheFile = WCF_DIR.'cache/cache.'.$cache.'.php';
+		$cacheBuilderFileName = WCF_DIR.'lib/system/cache/CacheBuilderScrollText.class.php';
+		WCF::getCache()->addResource($cache, $cacheFile, $cacheBuilderFileName);
+		WCF::getCache()->clearResource($cache);
+	}
+	
+	/**
+	 * Generates the response.
+	 */
+	protected function generateResponse() {
+		$this->response = '<li class="news-item">'.StringUtil::encodeHTML($this->newItem).'</li>';
+	}
+	
+	/**
+	 * Sends JSON-Encoded response.
+	 */
+	protected function sendResponse() {
+		header('Content-type: application/json');
+		echo JSON::encode($this->response);
+		exit;
+	}
+	
+	/**
+	 * 'Throws' an exception.
+	 * 
+	 * Effectively a failure in the AJAX request is intended.
+	 */
+	protected function throwException() {
+		$responseData = array(
+			'code' => 412,
+			'message' => 'Empty input'
+		);
+		
+		$statusHeader = 'HTTP/1.0 431 Bad Parameters';
+		
+		header($statusHeader);
+		header('Content-type: application/json');
+		echo JSON::encode($responseData);
+		exit;
+	}
+}
--- src/templates/scrollText.tpl
+++ src/templates/scrollText.tpl
@@ -3,9 +3,73 @@
     <li class="news-item">{$text}</li>
 {/foreach}
 </ul>
+{if $this->user->userID}
+<form id="news-item-form">
+	<div class="border content">
+        <div class="container-1">
+            <fieldset>
+                <div class="formElement" id="newsItemDiv">
+                    <div class="formFieldLabel">
+                        <label for="newsItem">{lang}wcf.global.newsItem{/lang}</label>
+                    </div>
+                    <div class="formField">
+                        <input type="text" class="inputText" id="newsItem" name="newsItem" value="" />
+                    </div>
+                </div>
+            </fieldset>
+        </div>
+    </div>
+    <div class="formSubmit" style="float: right;">
+        <input type="submit" name="send" accesskey="s" value="{lang}wcf.global.button.submit{/lang}" tabindex="{counter name='tabindex'}" />
+        {@SID_INPUT_TAG}
+    </div>
+</form>
+{/if}
 <script type="text/javascript">
 //<![CDATA[
+	var emptyError = '{lang}wcf.global.error.empty{/lang}';
 	jQuery(document).ready(function($) {
+		{if $this->user->userID}
+			$('#news-item-form').submit(function(event) {
+				event.preventDefault();
+				var $value = $('#newsItem').val();
+				var options = {
+					autoSend: false,
+					data: {
+						value: $value 
+					},
+					dataType: 'json',
+					after: null,
+					init: null,
+					jsonp: 'callback',
+					async: true,
+					failure: function(data, textStatus, jqXHR) {
+						$('#newsItem').parent('.formField').append('<p class="innerError">' + emptyError + '</p>');
+						$('#newsItemDiv').addClass('formError');
+					},
+					showLoadingOverlay: false,
+					success: function(data, textStatus, jqXHR) {
+						$('#js-news').append(data);
+						$('#js-news').ticker();
+					},
+					suppressErrors: false,
+					type: 'POST',
+					url: 'index.php?action=ScrollText&t=' + SECURITY_TOKEN + SID_ARG_2ND,
+					aborted: null,
+					autoAbortPrevious: false
+				};
+				$.ajax({
+					data: options.data,
+					dataType: options.dataType,
+					jsonp: options.jsonp,
+					async: options.async,
+					type: options.type,
+					url: options.url,
+					success: options.success,
+					error: null
+				});
+			});
+		{/if}
 		$('#js-news').ticker({
 			speed: 0.10,           // The speed of the reveal
 	        ajaxFeed: false,       // Populate jQuery News Ticker via a feed
